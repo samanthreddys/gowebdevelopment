83a4ed33e8641a2dba4fdc624a8db731e5ca1e70
Samanth Reddy Chintakuntla <samanthreddys@gmail.com>
2018-10-14 21:24:13 -0500

    added runner file

--- main.go
+++ main.go
@@ -5,14 +5,13 @@ import (
 	"net/http"
 )
 
-func main(){
-	http.HandleFunc("/",handlerFunc)
-	http.ListenAndServe(":3000",nil)
-	
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
+
 }
 
-func handlerFunc(w http.ResponseWriter,r *http.Request){
-	fmt.Fprint(w,"<h1>Welcome to Web Development</h1>")
-
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to Web Server Development</h1>")
 
 }

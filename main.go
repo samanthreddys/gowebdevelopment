70468a5f73e55d6eef2483a01e6f4dee1c4eee62
Samanth Reddy Chintakuntla <samanthreddys@gmail.com>
2018-10-15 18:34:02 -0500

    added conent type

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 }
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to Web Server Development</h1>")
 
 }

74fc834708fb393f81bdc700674f2c06145cff1f
Samanth Reddy Chintakuntla <samanthreddys@gmail.com>
2018-10-16 21:14:31 -0500

    excercises3task2

--- main.go
+++ main.go
@@ -7,21 +7,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-/* func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to Web Server Development</h1>")
-
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch please email to <a href= \"mailto:test@test.com\"> test@test.com.</a>")
-
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>We could not find the page you requested for:(</h1><p>Please email us if you keep being sent to invalid page.</p>")
-	}
-
-} */
-
 func faqs(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "This ia a FAQ's Page. Work in Progress !")
@@ -37,11 +22,19 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch please email to <a href= \"mailto:test@test.com\"> test@test.com.</a>")
 }
 
+func notfound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>We could not find the page you requested for:(</h1><p>Please email us if you keep being sent to invalid page.</p>")
+}
+
 func main() {
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notfound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faqs", faqs)
+
 	http.ListenAndServe(":3000", r)
 
 }

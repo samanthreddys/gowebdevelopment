145d18957431e48aa1bbbbc7a23bd3ad44db1bfe
Samanth Reddy Chintakuntla <samanthreddys@gmail.com>
2018-10-29 19:46:52 -0500

    controller

---
+++ controllers/users.go
@@ -0,0 +1,35 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/samanthreddys/myweb.com/views"
+)
+
+//Users struct
+type Users struct {
+	NewView *views.View
+}
+
+//NewUsers function
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/signup.gohtml"),
+	}
+}
+
+//New  GET signup...
+// New Method to GET signup This is used to render a form when user click on a signup form
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	//fmt.Println("Is this called?")
+
+	u.NewView.Render(w, nil)
+
+}
+
+//Create Used to create user
+//POST SIGNUP
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is creating a user account")
+}
--- main.go
+++ main.go
@@ -5,19 +5,14 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/samanthreddys/myweb.com/controllers"
 	"github.com/samanthreddys/myweb.com/views"
 )
 
 var (
 	homeView    *views.View
 	contactView *views.View
-	signupView  *views.View
 )
-
-func faqs(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "This ia a FAQ's Page. Work in Progress !")
-}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -37,23 +32,20 @@ func notfound(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, "<h1>We could not find the page you requested for:(</h1><p>Please email us if you keep being sent to invalid page.</p>")
 }
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
-}
 
 func main() {
-	//var err error
+
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	signupView = views.NewView("bootstrap", "views/signup.gohtml")
+	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
 	r.NotFoundHandler = http.HandlerFunc(notfound)
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faqs", faqs)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/contact", contact).Methods("GET")
+
+	r.HandleFunc("/signup", usersC.New).Methods("GET")
+	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 
 	http.ListenAndServe(":3000", r)
 

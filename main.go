9e56a57c35db87b8075fe855e6436d127084d4bb
Samanth Reddy Chintakuntla <samanthreddys@gmail.com>
2018-11-01 20:00:33 -0500

    main

--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 	"github.com/samanthreddys/myweb.com/controllers"
 )
 
-func pagenotfound(w http.ResponseWriter, r *http.Request) {
+/* func pagenotfound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusNotFound)
 	http.ServeFile(w, r, "views/static/pagenotfound.gohtml")
 
-}
+} */
 
 func main() {
 	staticC := controllers.NewStatic()
@@ -21,8 +21,8 @@ func main() {
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
 	r.Handle("/contact", staticC.Contact).Methods("GET")
-	//r.Handle("/pagenotfound", staticC.PageNotFound).Methods("GET")
-	r.NotFoundHandler = http.HandlerFunc(pagenotfound)
+	r.Handle("/pagenotfound", staticC.PageNotFound).Methods("GET")
+	//r.NotFoundHandler = http.HandlerFunc(pagenotfound)
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 
